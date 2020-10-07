c9f8d71486fbc8d29412ab6f2de4d14ba79d8c0e
Leonardo Mercado <lamercadob1@gmail.com>
2020-10-07 15:36:05 -0500

    Hola mundo en react

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function HolaMundo(nombre){
+  var presentacion =<h2>hola, soy {nombre}</h2>
+
+  return presentacion;
+}
+
 function App() {
+  var nombre="Leonardo Mercado";
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +17,7 @@ function App() {
         <p>
           Editar en:  <code>src/App.js</code> and save to reload.
         </p>
+        {HolaMundo(nombre)}
         <a
           className="App-link"
           href="https://reactjs.org"
