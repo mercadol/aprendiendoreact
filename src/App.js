e434ac1d369c12ce6ca56381f763cf8aa54d02d2
Leonardo Mercado <lamercadob1@gmail.com>
2020-10-07 12:52:22 -0500

    Primer Commit de Blog con React

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Editar en:  <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
