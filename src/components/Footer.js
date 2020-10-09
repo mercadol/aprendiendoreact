4c39f59a58396fab0fe210544dca517c46caf671
Leonardo Mercado <lamercadob1@gmail.com>
2020-10-09 15:45:46 -0500

    Footerm Sidebar y Slider components

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+const Footer =() =>{
+    return(
+        <footer id="footer">
+            <div className="center">
+                <p>
+                    &copy; Máster en frameworks JS- Leonardo Mercado 
+                </p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
