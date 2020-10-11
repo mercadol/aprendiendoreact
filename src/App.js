b8ae7d37465a99b27bdb64c1c532f2ac65bc744d
Leonardo Mercado <lamercadob1@gmail.com>
2020-10-11 11:43:53 -0500

    COmponent Peliculas

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Header from './components/Header';
 import Slider from './components/Slider';
 import Sidebar from './components/Sidebar';
 import Footer from './components/Footer';
-import SeccionPruebas from './components/SeccionPruebas';
+import Peliculas from './components/Peliculas';
 
 
 function App() {
@@ -18,8 +18,7 @@ function App() {
     btn= {buttonString}
   />
     <div className="center">
-      
-      <SeccionPruebas />
+      <Peliculas></Peliculas>
 
       <Sidebar />
 
--- src/components/Peliculas.js
+++ src/components/Peliculas.js
@@ -1,12 +1,20 @@
 import React, {Component} from 'react';
-import MensajeEstatico from './MensajeEstatico';
 
 class Peliculas extends Component{
+    state={
+        peliculas:[
+            {titulo: 'Batman vs Superman', image: 'https://i.pinimg.com/originals/dd/c0/c0/ddc0c0c665086b10fd405b9530176a9b.jpg'},
+            {title: 'Gran torino', image: 'https://i.pinimg.com/originals/3e/51/ce/3e51cebd751e8e19cae7eec12df58988.jpg'},
+            {title: 'looper', image: 'https://pics.filmaffinity.com/Looper-874353819-large.jpg'}
+        ],
+        nombre: 'Leonardo Mercado'
+    };
     render(){
         return (
-            <div id="peliculas">
-                <h4>Soy el componente de Peliculas</h4>
-                <MensajeEstatico/>
+            <div id="content" className="peliculas">
+                <h2 className="subheader">Peliculas</h2>
+                <p>Seleccion de las peliculas favoritas de {this.state.nombre}</p>
+                
             </div>
         );
     }
