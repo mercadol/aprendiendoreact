55769f896644fc622dd6bbe4453ba359e20134fc
Leonardo Mercado <lamercadob1@gmail.com>
2020-10-12 13:28:25 -0500

    Error 404 y Rutas con condicion por url

--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Error from './components/Error';
 import MiComponente from './components/MiComponente';
 import Peliculas from './components/Peliculas';
 import SeccionPruebas from './components/SeccionPruebas'
@@ -15,6 +16,31 @@ class Router extends Component{
                     <Route exact path="/" component={Peliculas}/>
                     <Route exact path="/ruta-prueba" component={SeccionPruebas}/>
                     <Route exact path="/segunda-ruta" component={MiComponente}/>
+                    
+                    <Route exact path="/pruebas/:nombre/:apellido?" render={(props)=>{
+                        var nombre = props.match.params.nombre;
+                        var apellido = props.match.params.apellido;
+
+                        return(
+                            <div id="content">
+                                <h1 className="subheader">Pagina de pruebas</h1>
+                                <h2>
+                                {nombre &&
+                                <React.Fragment>
+                                {nombre}
+                                </React.Fragment>
+                                }
+                                {apellido &&
+                                <React.Fragment>
+                                {' '+apellido}
+                                </React.Fragment>
+                                }
+
+                                </h2>
+                            </div>
+                    )}}/>
+                    
+                    <Route component={Error}/>
                 </Switch>
             </BrowserRouter>
         );
