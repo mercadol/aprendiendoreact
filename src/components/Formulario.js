cdb0ca4bb147b234166975c6301d4c4b64e4a36e
Leonardo Mercado <lamercadob1@gmail.com>
2020-10-13 22:25:05 -0500

    Formularios de react

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,19 +1,77 @@
 import React, {Component} from 'react';
 import Sidebar from './Sidebar';
-import Slider from './Slider';
+//import Slider from './Slider';
 
 
 class Formulario extends Component{
+
+    nombreRef = React.createRef();
+    apellidosRef = React.createRef();
+    bioRef = React.createRef();
+    generoHombreRef = React.createRef();
+    generoMujerRef = React.createRef();
+    generoOtroRef = React.createRef();
+
+    state= {
+        user:{}
+    };
+
+    
+    recibirFormulario = (e) =>{
+        e.preventDefault();
+        var genero= 'hombre';
+        if(this.generoHombreRef.current.checked){
+            genero= 'hombre';
+        }else if(this.generoMujerRef.current.checked){
+            genero= 'mujer';
+        }else{
+            genero= 'otro';
+        }
+        var user ={
+            nombre: this.nombreRef.current.value,
+            apellidos: this.apellidosRef.current.value,
+            bio: this.bioRef.current.value,
+            genero: genero
+        };
+
+        this.setState({
+            user: user
+        });
+    }
+
     render(){
         return (<div id="formulario">
-        <Slider title="Formulario"
-                size="slider-small"
-        />
+
         <div className="center">
 
             <div id="content">
-            {/**Listados de articulas from back**/}
+                <h1 className="subheader">Formulario</h1>
+                <form className="mid-form" onSubmit={this.recibirFormulario}>
+                    <div className="form-grup">
+                        <label htmlFor="nombre" >Nombre</label>
+                        <input type="text" name="nombre" ref={this.nombreRef} />
+                    </div>
+                    
+                    <div className="form-grup">
+                        <label htmlFor="apellidos">Apellidos</label>
+                        <input type="text" name="apellidos" ref={this.apellidosRef} />
+                    </div>
+                    
+                    <div className="form-grup">
+                        <label htmlFor="bio">Biografia</label>
+                        <textarea name="bio" ref={this.bioRef}></textarea>
+                    </div>
+
+                    <div className="form-grup">
+                        <input type="radio" name="genero" value="hombre" ref={this.generoHombreRef} /> Hombre
+                        <input type="radio" name="genero" value="mujer" ref={this.generoMujerRef} /> Mujer
+                        <input type="radio" name="genero" value="otro" ref={this.generoOtroRef} /> Otro
+                    </div>
+                    <div className="clearfix"></div>
+                    <input type="submit" value="Enviar" className="btn btn-success" />
+                </form>
             </div>
+            
             <Sidebar
                 blog="false" />
         </div>
